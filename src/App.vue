0bff505ea214eb0d1ee7fdd8e0c95cff21fb8cee
Azat Ayupov <azat.web@gmail.com>
2021-10-13 18:41:01 +0300

    feat: make traffic-lights application

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,172 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="traffic-light-wrapper">
+    <div class="rope"></div>
+    <div :class="[light.color, { blink: blink }]" class="traffic-light wind">
+      <div class="light red-light"></div>
+      <div class="light yellow-light"></div>
+      <div class="light green-light"></div>
+    </div>
   </div>
-  <router-view />
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  background: radial-gradient(134% 182% at 120% 34%, #fdfdfd 0%, #2783c4 100%);
 }
 
-#nav {
-  padding: 30px;
+.traffic-light-wrapper {
+  position: relative;
+  top: 15vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.rope {
+  width: 100vw;
+  height: 1.6vh;
+  background-color: black;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.traffic-light {
+  position: relative;
+  top: -2.77vh;
+  left: 15vw;
+
+  display: flex;
+  width: 26.8vh;
+  height: 64.4vh;
+  padding-bottom: 3.3vh;
+
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 2.1vh;
+
+  background-image: url("./assets/traffic-light.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+.wind {
+  animation-name: wind;
+  animation-timing-function: linear;
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  transform-origin: 50% 0;
+}
+@keyframes wind {
+  25% {
+    transform: rotate(3deg);
+  }
+  60% {
+    transform: rotate(0);
+  }
+  85% {
+    transform: rotate(-2deg);
+  }
+}
+
+.light {
+  width: 12.7vh;
+  height: 12.7vh;
+
+  background-image: url("./assets/light-off.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.red > .red-light {
+  background-image: url("./assets/red-light.svg");
+}
+.yellow > .yellow-light {
+  background-image: url("./assets/yellow-light.svg");
+}
+.green > .green-light {
+  background-image: url("./assets/green-light.svg");
+}
+
+.blink > .light {
+  animation-name: blink;
+  animation-timing-function: linear;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+}
+@keyframes blink {
+  50% {
+    background-image: url("./assets/light-off.svg");
+  }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      colorList: [
+        { color: "red", time: 5000, isBlinking: true, blinkingTime: 3000 },
+        { color: "yellow", time: 3000, isBlinking: false, blinkingTime: 0 },
+        { color: "green", time: 5000, isBlinking: true, blinkingTime: 3000 },
+      ],
+      light: { color: "" },
+      next: null,
+      order: 1,
+      blink: false,
+    };
+  },
+
+  methods: {
+    changeColor() {
+      this.light = this.colorList.find(
+        (light) => light.color === this.$route.params.color
+      );
+
+      const currentColorIndex = this.colorList.indexOf(this.light);
+      if (currentColorIndex === 0) this.order = 1;
+      if (currentColorIndex === this.colorList.length - 1) this.order = -1;
+      const next = this.colorList[currentColorIndex + this.order];
+
+      if (this.light.isBlinking) {
+        const timeLeftToBlinking = this.light.time - this.light.blinkingTime;
+        setTimeout(() => {
+          this.blink = true;
+        }, timeLeftToBlinking);
+      }
+      setTimeout(() => {
+        this.blink = false;
+        this.$router.push({
+          name: "traffic-light",
+          params: { color: next.color },
+        });
+      }, this.light.time);
+    },
+
+    isEmpty(string) {
+      if (string === "" || string === undefined || string === null) return true;
+      return false;
+    },
+  },
+
+  watch: {
+    $route() {
+      if (this.isEmpty(this.$route.params.color)) {
+        let newColor;
+        if (!this.isEmpty(localStorage.color)) newColor = localStorage.color;
+        else newColor = this.colorList[0].color;
+
+        this.$router.push({
+          name: "traffic-light",
+          params: { color: newColor },
+        });
+      } else {
+        localStorage.color = this.$route.params.color;
+        this.changeColor();
+      }
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import App from "../App.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/:color(green|yellow|red)?",
+    name: "traffic-light",
+    component: App,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/About.vue");
-    },
+    path: "/:not-color(.*)*",
+    redirect: "/",
   },
 ];
 
